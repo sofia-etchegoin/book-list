548133cde5ed4076c45ff814c5ce18faaca8aa9e
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2024-01-09 14:43:13 +1300

    created association between users and books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title_name, :author_name, :comment)
+      params.require(:book).permit(:title_name, :author_name, :comment, :user_id)
     end
 end
