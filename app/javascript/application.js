9b85b9feba10c20add89dd74051895dfc6329830
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2024-01-09 14:09:39 +1300

    styled the edit profile page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import '@hotwired/turbo-rails'
+//import '@hotwired/turbo-rails'
 
-import 'controllers'
+//import 'controllers'
+
+import { Application } from '@hotwired/stimulus'
+import { Turbo } from '@hotwired/turbo-rails'
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus = application
+Turbo.start()
+
+export { application }
